02d44badd524e583a4e46e5132889d242f3886d0
Daisy Yi <daisyyi@rocketmail.com>
2022-09-19 15:01:15 -0700

    commit tree 726bb77274d56f270ce7caed45dd7a17f84478d4

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const userInfo = {
+    name: "Daisy",
+    email: "dyi@gmail.com",
+};
+
+const submitData = {
+    method : "POST",
+    headers : {
+        "Content-Type" : "applcation/json",
+        "Accept" : "application/json",
+    },
+    body : JSON.stringify(userInfo),
+};
+
+fetch ("http://localhost:3000/users", submitData)
+.then(function(response) {
+    return response.json();
+})
+.then(function(id) {
+    console.log(id);
+})
+.catch(function(error) {
+    alert("uh-oh");
+    console.log(error.messagE);
+});
+
